49452e6a0283be3f565e15c4f0196465ace7fbbd
Daffa Alfandy <m.daffaalfandy@gmail.com>
2026-04-20 11:20:54 +0700

    update origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: [
+    "192.168.18.55",
+    "macmini.local",
+    "*.local",
+  ],
 };
 
 export default nextConfig;
